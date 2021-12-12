f9e90897c501c87ec3206e20df9c7e59df142d66
PARKBORAM <parkboram@PARKBORAMui-MacBookPro.local>
2021-12-12 20:57:37 +0900

    third

--- minahzzang/settings.py
+++ minahzzang/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-3wauh&icmjv6pn%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = [ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']]
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
